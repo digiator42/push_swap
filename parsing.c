2cdbb316ef8ba4e1b44aa31f652f235221937cdf
Ahmed Hassan <digiator2020@gmail.com>
2023-01-21 19:41:27 +0400

    string args to linked list

--- parsing.c
+++ parsing.c
@@ -74,12 +74,10 @@ int parsing(int ac, char **av)
 	if (!is_valid_num(av) || !is_not_dup(av) || !is_not_max(av))
 		return 0;
 	i = 0;
-	first = ft_lstnew(ft_atoi(av[i++]));
-	while(i < j)
-		ft_lstadd_back(&first, ft_lstnew(ft_atoi(av[i++])));
-	while(first){
-		printf("val -> %d\n", (first)->value);
+	first = ft_lstnew(ft_atoi(*av));
+	while(*av)
+		ft_lstadd_back(&first, ft_lstnew(ft_atoi(*av++)));
+	while(first)
 		first = first->next;
-	}
 	return 1;
 }
