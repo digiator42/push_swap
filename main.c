3aa816c12da9552cefd7df7098a69eee7fa0fd48
Ahmed Hassan <digiator2020@gmail.com>
2023-02-05 17:46:50 +0400

    max int in 1 args

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ahassan <ahassan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 22:49:58 by ahassan           #+#    #+#             */
-/*   Updated: 2023/02/05 17:43:12 by ahassan          ###   ########.fr       */
+/*   Updated: 2023/02/05 17:46:05 by ahassan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,7 @@ int	main(int ac, char **av)
 	static char	*str;
 	int			i;
 
-	if (ac <= 2)
+	if (ac <= 1)
 		return (0);
 	i = 0;
 	while (++i < ac)
